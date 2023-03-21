b79d67b072e13b8ddf5898baf8219753247251ea
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-03-21 05:53:11 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Include any necessary libraries */
+#include <stdio.h>
+
+/* Function prototypes */
+int my_function(int arg1, char arg2);
+void another_function(void);
+int yet_another_function(float arg3);
+
+/* Prototype for _putchar */
+int _putchar(char c);
+
+#endif /* MAIN_H */
