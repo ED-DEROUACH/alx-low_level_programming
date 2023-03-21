1812d4b79faee255a03a53cde3950c4aa1c0fce5
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-03-21 11:05:29 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_last_digit - function that prints last digit
- * @n: function parameter
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to find the last digit of
  *
- * Return: last_digit
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
@@ -11,6 +11,6 @@ int last_digit;
 if (n < 0)
 n = -n;
 last_digit = n % 10;
-putchar(last_digit + '0');
+_putchar(last_digit + '0');
 return (last_digit);
 }
