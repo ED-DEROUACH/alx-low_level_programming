d86dfe5060f2befbd29170b45cfcfe570b640cd6
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-03-22 08:53:22 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
 * main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
 
 {
@@ -11,7 +12,6 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	
 	i = 0;
 	/*
 	while (i < 10)
