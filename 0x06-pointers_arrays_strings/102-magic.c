77a6d5d8f03d205d4550769c24d502708f98bed6
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-03-29 11:43:18 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,37 +1,20 @@
 #include <stdio.h>
 
 /**
-* main - Entry point for the program
-*
-* This function initializes an
-* integer variable, an integer array and a
-* pointer, and then performs
-* some operations on them.
-*
-* @n;: Déclare une variable n de type int.
-* @a[5];: Déclare un tableau a de cinq éléments de type int.
-* @*p;: Déclare un pointeur p de type int*.
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n;
-int a[5];
+int a[5] = {98, 402, 986, 1024, 4096};
 int *p;
 
-a[2] = 1024;
-p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
+p = &a[0];
+
+/* Add your code here */
+
+printf("a[2] = %d\n", *(p + 2));
+
 return (0);
 }
