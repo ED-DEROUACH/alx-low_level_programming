8983b9face8205bf244c0cac60ea52f27a6fae0a
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-03-29 11:27:56 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,16 +8,30 @@
 * pointer, and then performs
 * some operations on them.
 *
+* int n;: Déclare une variable n de type int.
+* int a[5];: Déclare un tableau a de cinq éléments de type int.
+* int *p;: Déclare un pointeur p de type int*.
+*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-int a[] = {0, 1, 2, 3, 4};
-int *p;
+  int n;
+  int a[5];
+  int *p;
 
-p = &a[2];
-*p = 98;
-
-printf("a[2] = %d\n", a[2]);
-return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
