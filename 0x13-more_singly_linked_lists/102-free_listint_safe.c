f9934ca88fd77c50744c295e72c719c43e054512
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-04-28 05:06:12 -0700

    13. Free (safe version)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
+ */
+size_t free_listint_safe(listint_t **j)
+{
+	size_t lol = 0;
+	int ffid;
+	listint_t *pmet;
+
+	if (!j || !*j)
+		return (0);
+
+	while (*j)
+	{
+		ffid = *j - (*j)->next;
+		if (ffid > 0)
+		{
+			pmet = (*j)->next;
+			free(*j);
+			*j = pmet;
+			lol++;
+		}
+		else
+		{
+			free(*j);
+			*j = NULL;
+			lol++;
+			break;
+		}
+	}
+
+	*j = NULL;
+
+	return (lol);
+}
+
---
+++ 0x13-more_singly_linked_lists/102-main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *testo;
+    listint_t *king;
+    listint_t *polo;
+
+    king = NULL;
+    add_nodeint(&king, 0);
+    add_nodeint(&king, 1);
+    add_nodeint(&king, 2);
+    add_nodeint(&king, 3);
+    add_nodeint(&king, 4);
+    add_nodeint(&king, 98);
+    add_nodeint(&king, 402);
+    add_nodeint(&king, 1024);
+    print_listint_safe(king);
+    testo = NULL;
+    polo = add_nodeint(&testo, 0);
+    add_nodeint(&testo, 1);
+    add_nodeint(&testo, 2);
+    add_nodeint(&testo, 3);
+    add_nodeint(&testo, 4);
+    polo->next = add_nodeint(&testo, 98);
+    add_nodeint(&testo, 402);
+    add_nodeint(&testo, 1024);
+    print_listint_safe(testo);
+    free_listint_safe(&king);
+    free_listint_safe(&testo);
+    printf("%p, %p\n", (void *)king, (void *)testo);
+    return (0);
+}
+
