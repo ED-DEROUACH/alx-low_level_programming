34cd28ab5b773336a213677deb4ea2fb287e6604
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-08-18 05:58:21 -0700

    up

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,24 +7,23 @@
  * @n: int element of the linked list
  * Return: number of elements
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node;
 
-    if (new_node == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node = malloc(sizeof(dlistint_t));
 
-    new_node->n = n;
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
     new_node->prev = NULL;
-    new_node->next = *head;
+	*head = new_node;
 
-    if (*head != NULL)
-        (*head)->prev = new_node;
-
-    *head = new_node;
-
-    return (new_node);
+	return (new_node);
 }
