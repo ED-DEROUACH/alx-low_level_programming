96becfe027d3fea0775549bfbc7e41c6d109f2bc
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-08-18 11:19:08 -0700

    up

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,19 +1,24 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list.
+ * sum_dlistint - find sum of elements in the list
  * @head: head of linked list
- * Retun: sum of data values
+ * Return: sum
  */
+
 int sum_dlistint(dlistint_t *head)
 {
-	int sum = 0;
 	dlistint_t *current;
+	dlistint_t *next;
+	int sum = 0;
 
-	for (current = head; current != NULL; current = current->next)
+	current = head;
+
+	while (current != NULL)
 	{
+		next = current->next;
 		sum += current->n;
+		current = next;
 	}
-
 	return (sum);
 }
