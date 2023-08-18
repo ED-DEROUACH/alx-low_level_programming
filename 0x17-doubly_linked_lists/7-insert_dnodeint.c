3989e62d5ffcc6e14d608159c7883f7c114129f5
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-08-18 21:10:40 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,38 +7,40 @@
  * @n: new node value
  * Return: inserted node
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *current;
-dlistint_t *new;
+	dlistint_t *current;
+	dlistint_t *new;
 
-if (h == NULL)
-return (NULL);
+	if (h == NULL)
+		return (0);
 
-current = *h;
+	current = *h;
 
-for (; idx != 0; idx--)
-{
-current = current->next;
-if (current == NULL)
-return (NULL);
-}
+	while (idx != 0)
+	{
+		current = current->next;
+		idx--;
+		if (current == NULL)
+			return (NULL);
+	}
 
-new = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 
-if (new == NULL)
-{
-free(new);
-return (NULL);
-}
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-new->n = n;
-new->next = current;
-new->prev = current->prev;
-if (current->prev != NULL)
-current->prev->next = new;
+	new->n = n;
+	new->next = current;
+	new->prev = current->prev;
+	if (current->prev != NULL)
+		current->prev->next = new;
 
-  /* TODO: Handle special case when idx is 0 and last index */
+	/*TODO: Handle special case when idx is 0 and last index*/
 
-return (current);
+	return (current);
 }
