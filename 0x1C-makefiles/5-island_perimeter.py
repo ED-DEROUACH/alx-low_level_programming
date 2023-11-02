01b61167c8df5cc669763ca6e360f1cfbcd45dad
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-11-02 15:07:06 -0700

    up

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,13 +9,10 @@ def island_perimeter(grid):
     width = len(grid[0])
     for i in range(height):
         for j in range(width):
-            if grid[i][j] == 0:
+            if grid[i][j] == 1:
+                cont += 4
                 if i > 0 and grid[i - 1][j] == 1:
-                        cont += 1
+                    cont -= 2
                 if j > 0 and grid[i][j - 1] == 1:
-                        cont += 1
-                if j < width - 1 and grid[i][j + 1] == 1:
-                        cont += 1
-                if i < height - 1 and grid[i + 1][j] == 1:
-                        cont += 1
+                    cont -= 2
     return cont
